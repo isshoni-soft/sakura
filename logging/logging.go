d0db4eb8d3032e01b63bc1720ce75554f90f4109
Andrew Burr <me@dragovorn.com>
2021-10-24 10:21:37 -0400

    add logfile and timestamps to logs

--- logging/logging.go
+++ logging/logging.go
@@ -1,13 +1,25 @@
 package logging
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+	"strconv"
+	"time"
+)
 
+var logFileChannel = make(chan string, 5)
 var defaultLogger = NewLogger("engine", 16)
+var dateLayout = "01-02-2006|15:04:05"
+var logFileName = "Sakura-" + time.Now().Format(dateLayout) + ".log"
 
 type Logger struct {
 	loggerChannel chan string
 
 	prefix string
+}
+
+func init() {
+	go logFileTick()
 }
 
 func GetLogger() *Logger {
@@ -16,7 +28,7 @@ func GetLogger() *Logger {
 
 func NewLogger(prefix string, buffer int) *Logger {
 	result := new(Logger)
-	result.prefix =  "sakura:" + prefix + "| "
+	result.prefix =  "[" + time.Now().Format(dateLayout) +"]: sakura:" + prefix + "| "
 	result.loggerChannel = make(chan string, buffer)
 
 	go result.loggerTick()
@@ -27,6 +39,8 @@ func NewLogger(prefix string, buffer int) *Logger {
 func (l Logger) loggerTick() {
 	for str := range l.loggerChannel {
 		fmt.Println(str)
+
+		logFileChannel <- str // now that we've logged the line lets queue it for adding to logfile
 	}
 }
 
@@ -50,4 +64,33 @@ func (l Logger) Format(str ...string) (result string) {
 
 func (l Logger) Log(str ...string) {
 	l.loggerChannel <- l.Format(str...)
+}
+
+func logFileTick() {
+	num := 1
+
+	for _, err := os.Stat(logFileName); os.IsExist(err); {
+		logFileName ="Sakura-" + time.Now().Format(dateLayout) + "-" + strconv.FormatInt(int64(num), 10) + ".log"
+	}
+
+	f, err := os.Create(logFileName)
+
+	if err != nil {
+		panic(err)
+	}
+
+	defer func(f *os.File) {
+		err := f.Close()
+
+		if err != nil {
+			panic(err)
+		}
+	}(f)
+
+	for str := range logFileChannel {
+		_, err := f.WriteString(str + "\n")
+		if err != nil {
+			return
+		}
+	}
 }
